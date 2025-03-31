0452abc57b294a20024b73fc2a197e59d3f94406
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-03-31 22:56:50 +0530

    Delete backend/models directory

--- backend/models/CodeSnippet.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const CodeSnippetSchema = new mongoose.Schema({
-    code: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("CodeSnippet", CodeSnippetSchema);
