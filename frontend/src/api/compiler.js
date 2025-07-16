ee801fc3837e8b9b5cb54fb6cdfa6279989517d8
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-07-16 22:30:58 +0530

    Update compiler.js

--- frontend/src/api/compiler.js
+++ frontend/src/api/compiler.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://javascriptplayground-4epl.onrender.com/api";
 
 export const runCode = async (code) => {
     try {
